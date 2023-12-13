54d7e0e98da9ada7c77d65c70fd619811eb9593d
Trinh Viet Hoang <trinhviethoang16@gmail.com>
2023-12-13 20:25:24 +0700

    add terraform for infrastructure
    Setup SSH Key and Digital Ocean Droplet

---
+++ infrastructure/develop/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.34.0"
+    }
+  }
+}
+
+resource "digitalocean_droplet" "fullstack-test" {
+  image  = "ubuntu-22-04-x64"
+  name   = "fullstack-test"
+  region = "sgp1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    digitalocean_ssh_key.default.fingerprint
+  ]
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "default"
+  public_key = file("../../.ssh/id_rsa.pub")
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
