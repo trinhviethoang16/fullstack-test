2dfeb83d0ddcc43a9e5009aeefef9e4b4e5722dd
Trinh Viet Hoang <trinhviethoang16@gmail.com>
2023-12-13 12:44:38 +0700

    VHT-29 | Setup Loopback4 for Fullstack Test

---
+++ backend/src/models/figure.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Figure extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  shape: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  color: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  symbol: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  meansurement: number;
+
+
+  constructor(data?: Partial<Figure>) {
+    super(data);
+  }
+}
+
+export interface FigureRelations {
+  // describe navigational properties here
+}
+
+export type FigureWithRelations = Figure & FigureRelations;
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'boolean',
+    default: false,
+  })
+  isAdmin?: boolean;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
